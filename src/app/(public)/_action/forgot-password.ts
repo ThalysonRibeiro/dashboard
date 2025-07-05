b6304e38409b359debe15e8aa4e31e581e8d32cc
THALYSON RAFAEL <rafinha.head@gmail.com>
2025-07-05 20:55:41 -0300

    feat:action  forgot password

---
+++ src/app/(public)/_action/forgot-password.ts
@@ -0,0 +1,40 @@
+'use server'
+
+import serverApi from '@/lib/serverApi'
+import { AxiosError } from 'axios';
+import { FormForgotPasswordData } from '../hooks/use-form-forgot-password';
+
+export async function forgotPasswordServerAction(data: FormForgotPasswordData) {
+  try {
+    const response = await serverApi.post("/auth/forgot-password/dashboard", {
+      email: data.email,
+    });
+
+    if (!response.data) {
+      return {
+        success: false,
+        error: response.data.error || 'Erro ao enviar link de recuperação'
+      };
+    }
+
+    return {
+      success: true,
+      message: 'Link de recuperação enviado com sucesso!'
+    };
+
+  } catch (error) {
+    if (error instanceof AxiosError) {
+      const errorMessage = error.response?.data?.message || 'Erro desconhecido';
+
+      return {
+        success: false,
+        error: errorMessage
+      };
+    }
+
+    return {
+      success: false,
+      error: 'Erro interno do servidor'
+    };
+  }
+}
